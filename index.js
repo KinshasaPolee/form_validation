42c38bc91b59220c7ff03feceb7f906671fd02cb
KinshasaPolee <kpolee1@gmail.com>
2023-12-12 15:36:06 -0500

    added required to all input fields

---
+++ index.js
@@ -0,0 +1,9 @@
+document.getElementById('registration').addEventListener('submit', function(event){
+    let password = document.getElementById('password').value;
+    let passwordCheck = document.getElementById('passwordCheck').value; 
+    let passwordError = document.getElementById('passwordError');
+
+    if (password !== confirmPassword) {
+        
+    }
+} )
