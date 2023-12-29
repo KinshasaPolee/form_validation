05817518223fd8395cba12d589614eb89f61bac9
KinshasaPolee <kpolee1@gmail.com>
2023-12-29 14:29:49 -0500

    added username validation

--- index.js
+++ index.js
@@ -7,106 +7,119 @@ document.addEventListener("DOMContentLoaded", function () {
         event.preventDefault();
         errorDisplay.innerHTML = "";
 
-        const username = registrationForm.username.value.trim().toLowerCase();
-        if (username === "" || username.length < 4 || !hasTwoUniqueChars(username) || hasSpecialChars(username)) {
-            displayError("Invalid username.");
-            return;
+        // const username = registrationForm.username.value.trim().toLowerCase();
+        // if (username === "" || username.length < 4 || !hasTwoUniqueChars(username) || hasSpecialChars(username)) {
+        //     displayError("Invalid username.");
+        //     return;
+        // }
+        // email validation
+        function validateForm() {
+            var emailValue = document.getElementById("email").value;
+            var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            if (!emailRegex.test(emailValue)) {
+                alert("Invalid email format!");
+                return false;
+            }
+            if (emailValue.endsWith("@example.com")) {
+                alert("Email from 'example.com' domain is not allowed!");
+                return false;
+            }
+            return true;
         }
+        // const email = registrationForm.email.value.trim().toLowerCase();
+        // if (!isValidEmail(email) || email.endsWith("@example.com")) {
+        //     displayError("Invalid email.");
+        //     return;
+        // }
 
-        const email = registrationForm.email.value.trim().toLowerCase();
-        if (!isValidEmail(email) || email.endsWith("@example.com")) {
-            displayError("Invalid email.");
-            return;
-        }
+    //     const password = registrationForm.password.value;
+    //     const passwordCheck = registrationForm.passwordCheck.value;
+    //     if (!isValidPassword(password) || password !== passwordCheck) {
+    //         displayError("Invalid password.");
+    //         return;
+    //     }
 
-        const password = registrationForm.password.value;
-        const passwordCheck = registrationForm.passwordCheck.value;
-        if (!isValidPassword(password) || password !== passwordCheck) {
-            displayError("Invalid password.");
-            return;
-        }
+    //     if (!registrationForm.terms.checked) {
+    //         displayError("You must accept the Terms of Use.");
+    //         return;
+    //     }
 
-        if (!registrationForm.terms.checked) {
-            displayError("You must accept the Terms of Use.");
-            return;
-        }
+    //     const userData = { username, email, password };
+    //     storeUserData(userData);
 
-        const userData = { username, email, password };
-        storeUserData(userData);
+    //     registrationForm.reset();
 
-        registrationForm.reset();
+    //     displaySuccess("Registration successful!");
+    // });
 
-        displaySuccess("Registration successful!");
-    });
+    // loginForm.addEventListener("submit", function (event) {
+    //     event.preventDefault();
+    //     errorDisplay.innerHTML = "";
 
-    loginForm.addEventListener("submit", function (event) {
-        event.preventDefault();
-        errorDisplay.innerHTML = "";
+    //     const username = loginForm.username.value.trim().toLowerCase();
+    //     if (username === "" || !userExists(username)) {
+    //         displayError("Invalid username.");
+    //         return;
+    //     }
 
-        const username = loginForm.username.value.trim().toLowerCase();
-        if (username === "" || !userExists(username)) {
-            displayError("Invalid username.");
-            return;
-        }
+    //     const password = loginForm.password.value;
+    //     const storedPassword = getUserData(username).password;
+    //     if (password !== storedPassword) {
+    //         displayError("Incorrect password.");
+    //         return;
+    //     }
 
-        const password = loginForm.password.value;
-        const storedPassword = getUserData(username).password;
-        if (password !== storedPassword) {
-            displayError("Incorrect password.");
-            return;
-        }
+    //     loginForm.reset();
 
-        loginForm.reset();
+    //     const successMessage = "Login successful!";
+    //     if (loginForm.persist.checked) {
+    //         displaySuccess(successMessage + " (Keep me logged in)");
+    //     } else {
+    //         displaySuccess(successMessage);
+    //     }
+    // });
 
-        const successMessage = "Login successful!";
-        if (loginForm.persist.checked) {
-            displaySuccess(successMessage + " (Keep me logged in)");
-        } else {
-            displaySuccess(successMessage);
-        }
-    });
+    // function displayError(message) {
+    //     errorDisplay.innerHTML = `<div class="error">${message}</div>`;
+    // }
 
-    function displayError(message) {
-        errorDisplay.innerHTML = `<div class="error">${message}</div>`;
-    }
+    // function displaySuccess(message) {
+    //     errorDisplay.innerHTML = `<div class="success">${message}</div>`;
+    // }
 
-    function displaySuccess(message) {
-        errorDisplay.innerHTML = `<div class="success">${message}</div>`;
-    }
+    // function hasTwoUniqueChars(str) {
+    //     const uniqueChars = new Set(str);
+    //     return uniqueChars.size >= 2;
+    // }
 
-    function hasTwoUniqueChars(str) {
-        const uniqueChars = new Set(str);
-        return uniqueChars.size >= 2;
-    }
+    // function hasSpecialChars(str) {
+    //     const regex = /[!@#$%^&*(),.?":{}|<>]/;
+    //     return regex.test(str);
+    // }
 
-    function hasSpecialChars(str) {
-        const regex = /[!@#$%^&*(),.?":{}|<>]/;
-        return regex.test(str);
-    }
+    // function isValidEmail(email) {
+    //     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    //     return emailRegex.test(email);
+    // }
 
-    function isValidEmail(email) {
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        return emailRegex.test(email);
-    }
+    // function isValidPassword(password) {
+    //     const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]).{12,}$/;
+    //     return passwordRegex.test(password) && !password.toLowerCase().includes("password");
+    // }
 
-    function isValidPassword(password) {
-        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]).{12,}$/;
-        return passwordRegex.test(password) && !password.toLowerCase().includes("password");
-    }
+//     function storeUserData(userData) {
+//         const users = JSON.parse(localStorage.getItem("users")) || [];
+//         users.push(userData);
+//         localStorage.setItem("users", JSON.stringify(users));
+//     }
 
-    function storeUserData(userData) {
-        const users = JSON.parse(localStorage.getItem("users")) || [];
-        users.push(userData);
-        localStorage.setItem("users", JSON.stringify(users));
-    }
+//     function userExists(username) {
+//         const users = JSON.parse(localStorage.getItem("users")) || [];
+//         return users.some(user => user.username === username);
+//     }
 
-    function userExists(username) {
-        const users = JSON.parse(localStorage.getItem("users")) || [];
-        return users.some(user => user.username === username);
-    }
-
-    function getUserData(username) {
-        const users = JSON.parse(localStorage.getItem("users")) || [];
-        return users.find(user => user.username === username);
-    }
-});
+//     function getUserData(username) {
+//         const users = JSON.parse(localStorage.getItem("users")) || [];
+//         return users.find(user => user.username === username);
+//     }
+// });
