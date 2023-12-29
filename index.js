cb3c96049e4db233c34a44067add344618aa4a2f
KinshasaPolee <kpolee1@gmail.com>
2023-12-29 15:05:27 -0500

    pt.3 steps 1-4 complete

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
 function validateForm() {
-    var emailValue = document.getElementById("email").value;
-    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    // Email validation
+    let emailValue = document.getElementById("email").value;
+    let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
     if (!emailRegex.test(emailValue)) {
         alert("Invalid email format!");
         return false;
     }
+
     if (emailValue.endsWith("@example.com")) {
         alert("Email from 'example.com' domain is not allowed!");
         return false;
     }
+
+    // Password validation
+    let passwordValue = document.getElementById("password").value;
+    let passwordCheckValue = document.getElementById("passwordCheck").value;
+
+    if (passwordValue.length < 12) {
+        alert("Password must be at least 12 characters long!");
+        return false;
+    }
+
+    if (!/[a-z]/.test(passwordValue) || !/[A-Z]/.test(passwordValue) || !/\d/.test(passwordValue) || !/[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]/.test(passwordValue)) {
+        alert("Password must have at least one uppercase letter, one lowercase letter, one number, and one special character!");
+        return false;
+    }
+
+    if (passwordValue.toLowerCase().includes("password")) {
+        alert("Password cannot contain the word 'password'!");
+        return false;
+    }
+
+    if (passwordValue === emailValue) {
+        alert("Password cannot be the same as the email!");
+        return false;
+    }
+
+    if (passwordValue !== passwordCheckValue) {
+        alert("Passwords do not match!");
+        return false;
+    }
+
     return true;
 }
